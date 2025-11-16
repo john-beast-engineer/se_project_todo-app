88bbfa7c6e7873a507527edae6eb0285a97ac80f
BeastEngineer <john@bethebeast.ai>
2025-11-15 22:43:38 -0500

    deleted comments

--- components/Todo.js
+++ components/Todo.js
@@ -34,22 +34,18 @@ class Todo {
   getView() {
     this._element = this._getTemplate();
 
-    // Query ALL elements first
     const todoNameEl = this._element.querySelector(".todo__name");
     this._checkbox = this._element.querySelector(".todo__completed");
     const todoLabel = this._element.querySelector(".todo__label");
     const todoDate = this._element.querySelector(".todo__date");
     this._deleteButton = this._element.querySelector(".todo__delete-btn");
 
-    // Set text content
     todoNameEl.textContent = this._data.name;
     this._checkbox.checked = this._data.completed;
 
-    // Set id and for attributes
     this._checkbox.id = `todo-${this._data.id}`;
     todoLabel.setAttribute("for", `todo-${this._data.id}`);
 
-    // Handle date
     const dueDate = new Date(this._data.date);
     if (!isNaN(dueDate)) {
       todoDate.textContent = `Due: ${dueDate.toLocaleString("en-US", {
